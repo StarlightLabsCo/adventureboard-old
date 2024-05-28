045b9041873956b5b6c2a7e05991f418ec305176
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-27 19:15:19 -0700

    Don't use Next Image.

--- apps/client/src/components/users/Avatar.tsx
+++ apps/client/src/components/users/Avatar.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { cn } from '@/lib/tailwind/utils';
 
@@ -12,7 +11,7 @@ export function Avatar({ name, src, className }: Props) {
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Image src={src} height={100} width={100} className={cn('h-9 w-9 rounded-full', className)} alt={name} />
+        <img src={src} height={100} width={100} className={cn('h-9 w-9 rounded-full', className)} alt={name} />
       </TooltipTrigger>
       <TooltipContent>{name}</TooltipContent>
     </Tooltip>
