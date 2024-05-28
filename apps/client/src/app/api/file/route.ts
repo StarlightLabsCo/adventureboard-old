49de763acb4421ea17968cc5cfa127b92e3c2167
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-28 15:45:09 -0700

    fix checks.

--- apps/client/src/app/api/file/route.ts
+++ apps/client/src/app/api/file/route.ts
@@ -2,7 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-if (!process.env.R2_ACCESS_KEY_ID || !process.env.R2_SECRET_ACCESS_KEY || !process.env.R2_BUCKET_NAME || !process.env.R2_ACCOUNT_ID) {
+if (
+  !process.env.R2_ACCESS_KEY_ID ||
+  !process.env.R2_SECRET_ACCESS_KEY ||
+  !process.env.NEXT_PUBLIC_R2_BUCKET_NAME ||
+  !process.env.NEXT_PUBLIC_R2_ACCOUNT_ID
+) {
   throw new Error('Missing R2 credentials');
 }
 
