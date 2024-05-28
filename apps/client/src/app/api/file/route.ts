d6b615c1de8a420ce71a94f0b1128e4445ad8cbf
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-28 15:34:07 -0700

    Try new library for this.

--- apps/client/src/app/api/file/route.ts
+++ apps/client/src/app/api/file/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { AwsClient } from 'aws4fetch';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 if (!process.env.R2_ACCESS_KEY_ID || !process.env.R2_SECRET_ACCESS_KEY || !process.env.R2_BUCKET_NAME || !process.env.R2_ACCOUNT_ID) {
   throw new Error('Missing R2 credentials');
@@ -30,19 +31,22 @@ export async function POST(req: NextRequest) {
   const user = await userResponse.json();
   const { filename } = await req.json();
 
-  const r2 = new AwsClient({
-    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+  const s3Client = new S3Client({
     region: 'auto',
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+    },
   });
 
   const objectKey = `${user.id}/${Date.now()}_${filename}`;
-  const url = new URL(
-    `https://${process.env.NEXT_PUBLIC_R2_BUCKET_NAME}.${process.env.NEXT_PUBLIC_R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${objectKey}`,
-  );
-  url.searchParams.set('X-Amz-Expires', '3600');
+  const command = new PutObjectCommand({
+    Bucket: process.env.NEXT_PUBLIC_R2_BUCKET_NAME!,
+    Key: objectKey,
+  });
 
-  const signedUrl = await r2.sign(new Request(url, { method: 'PUT' }), { aws: { signQuery: true } });
+  const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
-  return NextResponse.json({ url: signedUrl.url });
+  return NextResponse.json({ url: signedUrl });
 }
