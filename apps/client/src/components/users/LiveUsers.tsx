7ec132ca1d70e7c4a2f7d9fbbfbdd65b8d1adace
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-27 19:05:18 -0700

    Fix avatar icon?

--- apps/client/src/components/users/LiveUsers.tsx
+++ apps/client/src/components/users/LiveUsers.tsx
@@ -2,69 +2,42 @@ import React from 'react';
 import { Avatar } from './Avatar';
 import { useWebsocketStore } from '@/lib/websocket';
 
-const adjectives = [
-  'Happy',
-  'Creative',
-  'Energetic',
-  'Lively',
-  'Dynamic',
-  'Radiant',
-  'Joyful',
-  'Vibrant',
-  'Cheerful',
-  'Sunny',
-  'Sparkling',
-  'Bright',
-  'Shining',
-];
-
-const animals = [
-  'Dolphin',
-  'Tiger',
-  'Elephant',
-  'Penguin',
-  'Kangaroo',
-  'Panther',
-  'Lion',
-  'Cheetah',
-  'Giraffe',
-  'Hippopotamus',
-  'Monkey',
-  'Panda',
-  'Crocodile',
-];
-
-function hashStringToNumber(str: string): number {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  return hash;
-}
-
-function generateRandomName(connectionId: string): string {
-  const hash = Math.abs(hashStringToNumber(connectionId));
-  const randomAdjective = adjectives[hash % adjectives.length];
-  const randomAnimal = animals[hash % animals.length];
-
-  return `${randomAdjective} ${randomAnimal}`;
-}
-
-function generateAvatarSrc(connectionId: string): string {
-  const hash = Math.abs(hashStringToNumber(connectionId));
-  return `https://liveblocks.io/avatars/avatar-${hash % 30}.png`;
-}
-
 function LiveUsersComponent() {
   const users = useWebsocketStore().useOthers();
   const currentUser = useWebsocketStore().useSelf();
   const hasMoreUsers = users.length > 3;
 
+  const getDefaultAvatarIndex = (id: string, discriminator: string) => {
+    const isMigratedUser = discriminator === '0';
+    return isMigratedUser ? (BigInt(id) >> BigInt(22)) % BigInt(6) : parseInt(discriminator) % 5;
+  };
+
   return (
     <div className="absolute top-1 right-1 flex gap-x-1 z-10 items-center ">
-      {currentUser && <Avatar name="You" src={generateAvatarSrc(currentUser.connectionId)} />}
+      {currentUser && (
+        <Avatar
+          name="You"
+          src={
+            currentUser.discordUser.avatar
+              ? `https://cdn.discordapp.com/avatars/${currentUser.discordUser.id}/${currentUser.discordUser.avatar}${currentUser.discordUser.avatar.startsWith('a_') ? '.gif' : '.png'}`
+              : `https://cdn.discordapp.com/embed/avatars/${getDefaultAvatarIndex(currentUser.discordUser.id, currentUser.discordUser.discriminator)}.png`
+          }
+        />
+      )}
       {users.slice(0, 3).map(({ connectionId }) => {
-        return <Avatar key={connectionId} name={generateRandomName(connectionId)} src={generateAvatarSrc(connectionId)} />;
+        const connection = useWebsocketStore().useOther(connectionId);
+        if (!connection) return null;
+        return (
+          <Avatar
+            key={connectionId}
+            name={connection.discordUser.global_name}
+            src={
+              connection.discordUser.avatar
+                ? `https://cdn.discordapp.com/avatars/${connection.discordUser.id}/${connection.discordUser.avatar}${connection.discordUser.avatar.startsWith('a_') ? '.gif' : '.png'}`
+                : `https://cdn.discordapp.com/embed/avatars/${getDefaultAvatarIndex(connection.discordUser.id, connection.discordUser.discriminator)}.png`
+            }
+          />
+        );
       })}
 
       {hasMoreUsers && <div className={''}>+{users.length - 3}</div>}
