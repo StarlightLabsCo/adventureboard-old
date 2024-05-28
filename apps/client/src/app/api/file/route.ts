87db326edcdf4c204417ab9b6fd64db6c6ed8960
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-28 15:47:14 -0700

    Fix route handler.

--- apps/client/src/app/api/file/route.ts
+++ apps/client/src/app/api/file/route.ts
@@ -38,7 +38,7 @@ export async function POST(req: NextRequest) {
 
   const s3Client = new S3Client({
     region: 'auto',
-    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    endpoint: `https://${process.env.NEXT_PUBLIC_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
       accessKeyId: process.env.R2_ACCESS_KEY_ID!,
       secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
