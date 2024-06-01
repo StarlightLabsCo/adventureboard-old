7948e396da203bf91126bf176ba221ace05845ff
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-31 22:07:02 -0700

    Change to tailwindcss.

--- apps/client/src/components/canvas/DMShare.tsx
+++ apps/client/src/components/canvas/DMShare.tsx
@@ -2,13 +2,7 @@ import { LiveUsers } from '@/components/users/LiveUsers';
 
 export function DMShare() {
   return (
-    <div
-      style={{
-        width: '100%',
-        textAlign: 'center',
-        minWidth: '80px',
-      }}
-    >
+    <div className="w-full flex items-centers gap-x-2 text-center">
       <LiveUsers />
       <button className="bg-primary text-foreground rounded-sm">Share</button>
     </div>
