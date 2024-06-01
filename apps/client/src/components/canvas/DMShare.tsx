3fccd306613d0a77c15c418280bed1cfee6085e4
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-31 23:36:22 -0700

    Remove shadcn button.

--- apps/client/src/components/canvas/DMShare.tsx
+++ apps/client/src/components/canvas/DMShare.tsx
@@ -1,11 +1,16 @@
+import discordSdk from '@/lib/discord';
 import { LiveUsers } from '@/components/users/LiveUsers';
-import { Button } from '@/components/ui/button';
 
 export function DMShare() {
   return (
     <div className="w-full flex items-centers justify-end gap-x-2 p-1 mr-1 h-10 z-[var(--layer-panels)]">
       <LiveUsers />
-      <Button className="bg-blue-500 h-full pointer-events-auto">Share</Button>
+      <button
+        onClick={() => discordSdk.commands.openInviteDialog()}
+        className="bg-blue-500 h-full pointer-events-auto cursor-pointer font-bold text-sm px-2 py-1 hover:bg-blue-600"
+      >
+        Share
+      </button>
     </div>
   );
 }
