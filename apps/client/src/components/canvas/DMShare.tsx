228259437371abcd2055d5aa584af1bdfd83400b
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-05-31 22:50:45 -0700

    Just trying z-index seeing if that works.

--- apps/client/src/components/canvas/DMShare.tsx
+++ apps/client/src/components/canvas/DMShare.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 
 export function DMShare() {
   return (
-    <div className="w-full flex items-centers justify-end gap-x-2 m-1">
+    <div className="w-full flex items-centers justify-end gap-x-2 m-1 z-20">
       <LiveUsers />
       <Button className="bg-blue-500">Share</Button>
     </div>
