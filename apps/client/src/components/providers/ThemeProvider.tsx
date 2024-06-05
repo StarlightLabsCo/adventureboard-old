4c194a68783f4559c8ba7cd116e38cc3eaf1a3c8
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-05 14:44:23 -0700

    Add more logging.

--- apps/client/src/components/providers/ThemeProvider.tsx
+++ apps/client/src/components/providers/ThemeProvider.tsx
@@ -6,10 +6,16 @@ import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  console.log(`[ThemeProvider] props: ${JSON.stringify(props)}`);
+
   const theme = useTheme();
+
+  console.log(`[ThemeProvider] theme: ${theme}`);
   const userPreferences = getUserPreferences();
+
+  console.log(`[ThemeProvider] userPreferences: ${JSON.stringify(userPreferences)}`);
   useEffect(() => {
-    console.log(`Setting theme to ${userPreferences.isDarkMode ? 'dark' : 'light'}`);
+    console.log(`[ThemeProvider] Setting theme to ${userPreferences.isDarkMode ? 'dark' : 'light'}`);
     if (userPreferences.isDarkMode) {
       theme.setTheme('dark');
     } else {
