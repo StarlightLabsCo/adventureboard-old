be0662df021d874df919e11c69aede62bc760e6c
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-05 15:11:18 -0700

    Force theme light.

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -2,5 +2,5 @@ const MillionLint = require('@million/lint');
 const million = require('million/compiler');
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: []
+  transpilePackages: [],
 };
--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
 import './globals.css';
 
-import { Providers } from '@/components/providers';
 import { cn } from '@/lib/tailwind/utils';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, 'bg-background')}>
-        <Providers>{children}</Providers>
+        <ThemeProvider attribute="class" forcedTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
