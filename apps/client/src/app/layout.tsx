6dd2c015d7d2544b957fbcdb14028dd9bb7bd37a
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-05 15:14:56 -0700

    Force dark theme.

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, 'bg-background')}>
-        <ThemeProvider attribute="class" forcedTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" forcedTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
