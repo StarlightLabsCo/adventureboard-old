e1af5deb19ab337d755a1c57a71f49c7458ba1a0
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-05 14:50:20 -0700

    Change background on body.

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import { Providers } from '@/components/providers';
+import { cn } from '@/lib/tailwind/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, 'bg-background')}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- apps/client/src/components/Loading.tsx
+++ apps/client/src/components/Loading.tsx
@@ -1,5 +1,5 @@
 // TODO: add nicer full page loading page with an animation in the middle that fades out before page transition, that'd be pretty slick
 
 export function Loading() {
-    return <div>Loading…</div>;
+  return <div>Loading…</div>;
 }
