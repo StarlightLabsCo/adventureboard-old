0cb4dc9f412632ae569b9563a55cded6afeca8f9
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-08 22:49:31 -0700

    Test new style panel

--- apps/client/src/components/canvas/panels/StylePanel.tsx
+++ apps/client/src/components/canvas/panels/StylePanel.tsx
@@ -4,6 +4,10 @@ export const StylePanel = track(() => {
   const editor = useEditor();
   const styles = useRelevantStyles();
 
+  if (editor.getCurrentToolId() === 'imageGen') {
+    return <div className="w-[164px] flex flex-col">Image Gen</div>;
+  }
+
   if (!styles || (editor.getCurrentToolId() === 'select' && editor.getSelectedShapes().length == 0))
     return <div className="w-[164px] pointer-events-none" />;
 
