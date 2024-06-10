b5648db00724c60df645c982f922c1302a576512
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-09 17:20:35 -0700

    Fix style.

--- apps/client/src/components/canvas/panels/StylePanel.tsx
+++ apps/client/src/components/canvas/panels/StylePanel.tsx
@@ -5,7 +5,7 @@ export const StylePanel = track(() => {
   const styles = useRelevantStyles();
 
   if (editor.getCurrentToolId() === 'imageGen') {
-    return <div className=".tlui-style-panel__wrapper w-[350px] h-[300px] flex flex-col">Image Gen</div>;
+    return <div className="tlui-style-panel__wrapper w-[350px] h-[300px] flex flex-col">Image Gen</div>;
   }
 
   if (!styles || (editor.getCurrentToolId() === 'select' && editor.getSelectedShapes().length == 0))
