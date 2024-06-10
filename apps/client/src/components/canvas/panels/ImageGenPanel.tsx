38a8cd0ba629de7099110db7e5311c1c1269ae90
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-09 18:25:53 -0700

    Test square for aspect ratio.

--- apps/client/src/components/canvas/panels/ImageGenPanel.tsx
+++ apps/client/src/components/canvas/panels/ImageGenPanel.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 export function ImageGenPanel() {
+  const aspectRatios: string[] = ['9/21', '9/16', '2/3', '4/5', '1/1', '5/4', '3/2', '16/9', '21/9'];
+  const [aspectRatio, setAspectRatio] = useState<string>(aspectRatios[4]);
+
   return (
     <div className="tlui-style-panel__wrapper w-[350px] flex flex-col gap-y-3 py-2 px-3">
       <div className="flex flex-col items-center gap-y-2">
         <div className="text-white">Image Size</div>
-        <div className="h-[100px]"></div>
+        <div className="h-[100px] flex">
+          <div className="h-[100px] w-[100px] rounded-[var(--radius-1)] border border-white flex items-center justify-center">
+            {aspectRatio}
+          </div>
+          <div className="flex flex-col">
+            <div></div>
+            <div className="w-full"></div>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col items-center gap-y-2">
         <div className="text-white">Prompt</div>
