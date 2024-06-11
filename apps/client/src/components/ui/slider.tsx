0c290ea6d1d23a1c2726acecf851bdd1b9946d68
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-11 16:47:13 -0700

    Add cursor grab.

--- apps/client/src/components/ui/slider.tsx
+++ apps/client/src/components/ui/slider.tsx
@@ -9,7 +9,7 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, R
       <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-[var(--color-primary)/20]">
         <SliderPrimitive.Range className="absolute h-full bg-[var(--color-primary)]" />
       </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className="cursor-pointer block h-4 w-4 rounded-full border border-[var(--color-primary)/50] bg-[var(--color-background)] shadow-[var(--shadow-1)] transition-colors focus-visible:outline-none focus-visible:ring-1 group-hover:ring-1  focus-visible:ring-[var(--color-focus)] disabled:pointer-events-none disabled:opacity-50" />
+      <SliderPrimitive.Thumb className="cursor-pointer block h-4 w-4 rounded-full border border-[var(--color-primary)/50] bg-[var(--color-background)] shadow-[var(--shadow-1)] transition-colors focus-visible:outline-none focus-visible:ring-1 group-hover:ring-1 focus-visible:ring-[var(--color-focus)] disabled:pointer-events-none disabled:opacity-50 active:cursor-grab" />
     </SliderPrimitive.Root>
   ),
 );
