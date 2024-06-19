07c3af9e71e4fbd0e8e55ea74a94ba13a72f7aa4
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-18 20:03:51 -0700

    Fix positioning of dialogue.

--- apps/client/src/components/ui/SystemSelectDialog.tsx
+++ apps/client/src/components/ui/SystemSelectDialog.tsx
@@ -1,7 +1,7 @@
 export function SystemSelectDialog() {
   return (
-    <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-[1000]">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-[var(--color-panel)] rounded-[var(--radius-2)] flex flex-col items-center">
+    <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 z-[1000] flex items-center justify-center">
+      <div className="w-[500px] h-[500px] shrink-0 bg-[var(--color-panel)] rounded-[var(--radius-2)] flex flex-col items-center">
         <h1 className="text-[var(--color-text-0)]">Select a system</h1>
         <div className="flex justify-between">
           <div>D&D 5e</div>
