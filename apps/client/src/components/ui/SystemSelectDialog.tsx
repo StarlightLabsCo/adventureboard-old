ba565d6430ac3e467a4bd1e1c17643e3da7661aa
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-18 23:11:40 -0700

    Add black cover when not selected

--- apps/client/src/components/ui/SystemSelectDialog.tsx
+++ apps/client/src/components/ui/SystemSelectDialog.tsx
@@ -1,21 +1,29 @@
+import { useState } from 'react';
+
 export function SystemSelectDialog() {
+  const [selectedSystem, setSelectedSystem] = useState<string>('');
+
+  const systems = ['d&d5e', 'pathfinder', 'daggerheart', 'other'];
+
+  const handleSelectSystem = (system: string) => {
+    setSelectedSystem(system);
+  };
+
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 z-[1000] flex items-center justify-center backdrop-blur-sm">
       <div className="w-3/4 h-3/4 shrink-0 bg-white rounded-[30px] flex flex-col items-center">
         <h1 className="text-[var(--color-text-0)]">Select a system</h1>
         <div className="flex justify-evenly items-center gap-x-5 w-full h-full px-2">
-          <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/dnd.webp" className="rounded-md" />
-          </div>
-          <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/pathfinder.webp" className="rounded-md" />
-          </div>
-          <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/daggerheart.webp" className="rounded-md" />
-          </div>
-          <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/other.webp" className="rounded-md" />
-          </div>
+          {systems.map((system) => (
+            <div
+              key={system}
+              className="relative w-full flex flex-col rounded-md hover:scale-105 transition-all duration-300 cursor-pointer"
+              onClick={() => handleSelectSystem(system)}
+            >
+              <img src={`/covers/${system}.webp`} className="rounded-md hover:scale-105 transition-all duration-300" />
+              {selectedSystem !== system && selectedSystem !== '' && <div className="absolute inset-0 bg-black/50 z-[1001]" />}
+            </div>
+          ))}
         </div>
       </div>
     </div>
