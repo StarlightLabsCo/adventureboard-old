4fe4ed5eafd4e2c1e921f72dc397afe68e970e6f
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-18 23:01:15 -0700

    Fix corners and spacing.

--- apps/client/src/components/ui/SystemSelectDialog.tsx
+++ apps/client/src/components/ui/SystemSelectDialog.tsx
@@ -3,22 +3,18 @@ export function SystemSelectDialog() {
     <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 z-[1000] flex items-center justify-center backdrop-blur-sm">
       <div className="w-3/4 h-3/4 shrink-0 bg-white rounded-[30px] flex flex-col items-center">
         <h1 className="text-[var(--color-text-0)]">Select a system</h1>
-        <div className="flex justify-evenly gap-x-5 w-full">
+        <div className="flex justify-evenly items-center gap-x-5 w-full h-full px-2">
           <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/dnd.webp" />
-            <div className="absolute bottom-0 left-1/2 text-white drop-shadow-md z-[1001]">D&D 5e</div>
+            <img src="/covers/dnd.webp" className="rounded-md" />
           </div>
           <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/pathfinder.webp" />
-            <div className="absolute bottom-0 left-1/2 text-lg text-white drop-shadow-md z-[1001]">Pathfinder</div>
+            <img src="/covers/pathfinder.webp" className="rounded-md" />
           </div>
           <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/daggerheart.webp" />
-            <div className="absolute bottom-0 left-1/2 text-lg text-white drop-shadow-md z-[1001]">Daggerheart</div>
+            <img src="/covers/daggerheart.webp" className="rounded-md" />
           </div>
           <div className="relative h-full w-full bg-gray-200 flex flex-col rounded-md">
-            <img src="/covers/other.webp" />
-            <div className="absolute bottom-0 left-1/2 text-lg text-white drop-shadow-md z-[1001]">Other</div>
+            <img src="/covers/other.webp" className="rounded-md" />
           </div>
         </div>
       </div>
