ad6aec45eed77535ee0617e66d069963f5ebe823
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-06-18 22:27:23 -0700

    Fix spacing.

--- apps/client/src/components/ui/SystemSelectDialog.tsx
+++ apps/client/src/components/ui/SystemSelectDialog.tsx
@@ -1,7 +1,7 @@
 export function SystemSelectDialog() {
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 z-[1000] flex items-center justify-center backdrop-blur-sm">
-      <div className="w-1/2 h-3/4 shrink-0 bg-white rounded-[30px] flex flex-col items-center">
+      <div className="w-3/4 h-3/4 shrink-0 bg-white rounded-[30px] flex flex-col items-center">
         <h1 className="text-[var(--color-text-0)]">Select a system</h1>
         <div className="flex justify-evenly w-full">
           <div>D&D 5e</div>
